6c8e5edfea2aac33a2ddd2c4b9bf47d65a2a05e2
Benedetto Simone <sim.benede@gmail.com>
2022-06-13 22:52:54 +0200

    add back to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,24 @@
     });
   }
 
+
+  /**
+   * Back to top button
+   */
+
+  var btn = $('#button-to-top');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
 })()
